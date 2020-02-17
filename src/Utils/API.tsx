2f67c3045973073ee572adc358c6c6a07493c347
Kam <anthony.kam@protonmail.eu>
2020-02-18 00:34:15 +0100

    API get and insects call type

--- src/Utils/API.tsx
+++ src/Utils/API.tsx
@@ -1,6 +1,11 @@
 
 const base_url = 'https://safe-anchorage-52970.herokuapp.com/';
 
+const objectToParam = (object : any) => {
+    var listParam : string[] = Object.keys(object).map(key => `${key}=${object[key]}` );
+    return listParam.join('&')
+}
+
 export const API = {
     post: (url : string, content: {})=>{
         return fetch(url, {
@@ -15,7 +20,17 @@ export const API = {
             return err
         });
     },
+    get: (url : string, content: {})=>{
+        return fetch(url + (Object.keys(content).length ? "?" + objectToParam(content) : ""), {
+            method: 'GET',
+        })
+        .catch((err) => {
+            console.log("ERROR:" + err)
+            return err
+        });
+    },
     url_login: base_url + 'api/user/login',
+    url_insects: base_url + 'api/insects',
     url_register: base_url + 'api/user/register',
 }
 
@@ -23,4 +38,11 @@ export const API = {
 export interface LoginType {
     token?: string;
     message?: string;
+}
+
+export interface InsectType {
+    id: number;
+    name: string;
+    threatlevel: number;
+    picture: string;
 }
