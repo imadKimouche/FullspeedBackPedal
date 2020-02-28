69e1b0349f3d715622bd360be8026cacaab0317d
Kam <anthony.kam@protonmail.eu>
2020-02-28 17:58:07 +0100

    expo

--- index.tsx
+++ index.tsx
@@ -2,8 +2,13 @@
  * @format
  */
 
+import { KeepAwake, registerRootComponent } from 'expo';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+if (__DEV__) {
+    KeepAwake.activate();
+  }
+
+  registerRootComponent(App);
