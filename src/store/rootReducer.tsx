b3fb1020d21228483776729e6cd906098b60bc55
Imad <imad.kimouche@epitech.eu>
2020-02-28 19:18:07 +0100

    [Chore] clean repo

--- src/store/rootReducer.tsx
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import userReducer from './reducers/userReducer';
-
-export const rootReducer = combineReducers({
-  userReducer
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
